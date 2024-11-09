c0f480383c07321f40d9a9eb0ed26b58b80ee758
paulo.decarvalho <paulo.decarvalho@almaviacx.com>
2024-11-09 11:33:48 +0100

    clear match done if admin

--- cjoli.Server/Extensions/UserExtension.cs
+++ cjoli.Server/Extensions/UserExtension.cs
@@ -4,7 +4,14 @@ namespace cjoli.Server.Extensions
 {
     public static class UserExtension
     {
+
         public static bool IsAdmin(this User? user)
+        {
+            return user?.Role == "ADMIN";
+        }
+
+
+        public static bool IsAdminWithNoCustomEstimate(this User? user)
         {
             var config = user?.Configs.FirstOrDefault();
             return user?.Role == "ADMIN" && (config == null || !config.UseCustomEstimate);
