b254abd2cb562b9167acf9471eae55cefc25aad9
paulo.decarvalho <paulo.decarvalho@almaviacx.com>
2025-04-14 15:17:17 +0200

    fix single usermatch

--- cjoli.Server/Services/CJoliService.cs
+++ cjoli.Server/Services/CJoliService.cs
@@ -298,7 +298,7 @@ namespace cjoli.Server.Services
             var match = matches.OrderBy(m => m.Time).LastOrDefault(m => (m.PositionA == positionA && m.PositionB == positionB) || (m.PositionB == positionA && m.PositionA == positionB));
             if (match != null)
             {
-                var userMatch = match.UserMatches.SingleOrDefault(u=>u.User!=null);
+                var userMatch = match.UserMatches.FirstOrDefault(u=>u.User!=null);
                 IMatch m = match.Done ? match : userMatch != null ? userMatch : match;
                 if (m.ScoreA > m.ScoreB || m.ForfeitB)
                 {
