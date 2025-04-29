26c2067064a4c6487d3b8b5e1beebb20c8bb4b46
paulo.decarvalho <paulo.decarvalho@almaviacx.com>
2025-04-29 09:30:01 +0200

    tamagui added

--- cjoli.client.mobile/app/_layout.tsx
+++ cjoli.client.mobile/app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
+import { TamaguiProvider, createTamagui } from "tamagui";
+import { defaultConfig } from "@tamagui/config/v4";
 
 export default function RootLayout() {
-  return <Stack />;
+  const config = createTamagui(defaultConfig);
+  return (
+    <TamaguiProvider config={config}>
+      <Stack />
+    </TamaguiProvider>
+  );
 }
--- cjoli.client.mobile/app/index.tsx
+++ cjoli.client.mobile/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { Button } from "tamagui";
 
 export default function Index() {
   return (
@@ -10,6 +11,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button theme="blue">Hello world</Button>
     </View>
   );
 }
