0d47d1c60e3d5d24c989580d40f2c39a88a0129c
paulo.decarvalho <paulo.decarvalho@almaviacx.com>
2025-04-29 18:15:30 +0200

    add tamagui

---
+++ cjoli.client.mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,62 @@
+import { Link, Tabs } from "expo-router";
+import React from "react";
+
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import { Button, useTheme } from "tamagui";
+import { Atom, AudioWaveform } from "@tamagui/lucide-icons";
+
+export default function TabLayout() {
+  const theme = useTheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: theme.red10.val,
+        tabBarStyle: {
+          backgroundColor: theme.background.val,
+          borderTopColor: theme.borderColor.val,
+        },
+        headerStyle: {
+          backgroundColor: theme.background.val,
+          borderBottomColor: theme.borderColor.val,
+        },
+        headerTintColor: theme.color.val,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Tab One",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+          headerRight: () => (
+            <Link href="/modal" asChild>
+              <Button mr="$4" bg="$green8" color="$green12">
+                Hello!
+              </Button>
+            </Link>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="two"
+        options={{
+          title: "Tab Two",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Tab Explore",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
--- cjoli.client.mobile/app/_layout.tsx
+++ cjoli.client.mobile/app/_layout.tsx
@@ -1,12 +1,110 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import { TamaguiProvider, createTamagui } from "tamagui";
-import { defaultConfig } from "@tamagui/config/v4";
+import * as SplashScreen from "expo-splash-screen";
+//import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { Provider } from "@/components/Provider";
+import { StatusBar } from "react-native";
+import { useTheme } from "tamagui";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const config = createTamagui(defaultConfig);
+  const [interLoaded, interError] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+  useEffect(() => {
+    if (interLoaded || interError) {
+      // Hide the splash screen after the fonts have loaded (or an error was returned) and the UI is ready.
+      SplashScreen.hideAsync();
+    }
+  }, [interLoaded, interError]);
+  console.log("inter", interLoaded);
+  console.log("error", interError);
+
+  if (!interLoaded && interError) {
+    return null;
+  }
+
+  const colorScheme = useColorScheme();
   return (
-    <TamaguiProvider config={config}>
-      <Stack />
-    </TamaguiProvider>
+    <Provider>
+      <RootLayoutNav />
+      {/*<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>*/}
+    </Provider>
+  );
+  /*return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );*/
+
+  //const colorScheme = useColorScheme();
+  /*const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }*/
+}
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+  const theme = useTheme();
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <StatusBar
+        barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
+      />
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen
+          name="modal"
+          options={{
+            title: "Tamagui + Expo",
+            presentation: "modal",
+            animation: "slide_from_right",
+            gestureEnabled: true,
+            gestureDirection: "horizontal",
+            contentStyle: {
+              backgroundColor: theme.background.val,
+            },
+          }}
+        />
+      </Stack>
+    </ThemeProvider>
   );
 }
