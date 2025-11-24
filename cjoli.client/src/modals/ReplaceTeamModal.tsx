6102d1b920606d867f11256c58225ac22a48d3f8
paulo.decarvalho <paulo.decarvalho@almaviacx.com>
2025-11-24 22:02:30 +0100

    fix test unit

--- cjoli.client/src/modals/ReplaceTeamModal.tsx
+++ cjoli.client/src/modals/ReplaceTeamModal.tsx
@@ -1,19 +1,12 @@
 import CJoliModal, { Field } from "../components/CJoliModal";
 import { Team } from "../models";
-import * as cjoliService from "../services/cjoliService";
 import useUid from "../hooks/useUid";
-import { useCJoli } from "../hooks/useCJoli";
-import { useToast } from "../hooks/useToast";
-import { useTranslation } from "react-i18next";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { useApi } from "../hooks/useApi";
 import { useSetting } from "../hooks/useSetting";
 
 const ReplaceTeamModal = ({ team }: { team?: Team }) => {
-  const { loadRanking } = useCJoli();
-  const { showToast } = useToast();
   const uid = useUid();
-  const { t } = useTranslation();
 
   const { tourney } = useSetting();
   const { getTeams, replaceTeam } = useApi();
@@ -37,14 +30,6 @@ const ReplaceTeamModal = ({ team }: { team?: Team }) => {
 
   const onSubmit = async ({ teamId }: { teamId: number }) => {
     doReplaceTeam({ team: team!, newTeam: teams.find((t) => t.id == teamId)! });
-    /*const ranking = await cjoliService.updateTeam(uid, team);
-    if (!ranking) {
-      showToast("danger", t("team.error.update", "Unable to update Team"));
-      return false;
-    } else {
-      loadRanking(ranking);
-      return true;
-    }*/
     return true;
   };
   return (
