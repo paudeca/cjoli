eeb6d0814530f23c15f82819b307ce43ec021e1c
paulo.decarvalho <paulo.decarvalho@almaviacx.com>
2026-04-02 09:30:16 +0200

    fix screen in fullcast

--- cjoli.client/src/hooks/useScreenSize.tsx
+++ cjoli.client/src/hooks/useScreenSize.tsx
@@ -1,6 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { CJoliContext } from "../contexts/CJoliContext";
 
 const useScreenSize = () => {
+  const ctx = useContext(CJoliContext);
+  if (!ctx) {
+    throw new Error("useCJoli has to be used within <CJoliProvider>");
+  }
+  const { state } = ctx;
+
   const [screenSize, setScreenSize] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
@@ -22,7 +29,7 @@ const useScreenSize = () => {
   }, []);
   return {
     ...screenSize,
-    isMobile: screenSize.width < 1200,
+    isMobile: screenSize.width < 1200 && state.page != "fullcast",
     isInFrame: window.self !== window.top,
   };
 };
