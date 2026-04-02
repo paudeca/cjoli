91e42993e5b1eaab701926c71ee1225f85e63404
paulo.decarvalho <paulo.decarvalho@almaviacx.com>
2026-04-02 21:44:40 +0200

    fix update user

--- cjoli.Server/Dtos/UserDto.cs
+++ cjoli.Server/Dtos/UserDto.cs
@@ -3,7 +3,7 @@
     public class UserDto
     {
         public int Id { get; set; }
-        public required string Login { get; set; }
+        public string? Login { get; set; }
         public string? Password { get; set; }
         public string? Role { get; set; }
         public List<UserConfigDto>? Configs { get; set; }
